2336f26f802cbf77452e83dc6826d511eee5e4e5
Gustavo <penarandagustavo5@gmail.com>
2025-08-26 21:37:00 -0500

    change domain exception to extend from IllegalArgument

--- domain/usecase/src/main/java/com/crediya/usecase/registeruser/EmailAlreadyInUseException.java
+++ domain/usecase/src/main/java/com/crediya/usecase/registeruser/EmailAlreadyInUseException.java
@@ -1,6 +1,6 @@
 package com.crediya.usecase.registeruser;
 
-public class EmailAlreadyInUseException extends RuntimeException {
+public class EmailAlreadyInUseException extends IllegalArgumentException {
     public EmailAlreadyInUseException(String message) {
         super(message);
     }
